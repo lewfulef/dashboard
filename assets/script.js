ca0efd892d94558122703db0250d29d19494adeb
Lewfu Lef <130715365+Lewfulef@users.noreply.github.com>
2023-08-26 16:11:47 -0400

    se agregaron cambios en el gráfico y un footer

--- assets/script.js
+++ assets/script.js
@@ -75,7 +75,9 @@ const getWeatherDetails = (cityName, lat, lon) => {
                     data: temperatures,
                     borderColor: "rgba(75, 192, 192, 1)",
                     backgroundColor: "rgba(75, 192, 192, 0.2)",
-                    borderWidth: 1
+                    borderWidth: 2,
+                    tension: 0.3,
+                    fill: true,
                 }]
             },
             options: {
